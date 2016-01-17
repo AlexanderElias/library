b15fa622fb74d94c002f5609b15133c63af7e288
AlexanderElias <alex.steven.elias@gmail.com>
2016-01-17 15:35:49 -0700

    Added new java class ViewManager

---
+++ java/android/ViewManager.java
@@ -0,0 +1,69 @@
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+public class ViewManager {
+      private Activity activity;
+      private FragmentManager fragmentManager;
+
+
+      public ViewManager (Activity activity) {
+            this.activity = activity;
+            fragmentManager = activity.getFragmentManager();
+      }
+
+      public void replaceContainer(Integer containerViewId, Fragment fragment, String tag) {
+
+            fragmentManager.beginTransaction()
+                    .replace(containerViewId, fragment, tag)
+                    .addToBackStack(null)
+                    .commit();
+            
+      }
+}
+
+/*
+                  Use this in your main activity to control back press
+
+            private FragmentManager fragmentManager = getFragmentManager();
+
+            @Override
+            public void onBackPressed() {
+                  if(fragmentManager.getBackStackEntryCount() == 0) {
+                        super.onBackPressed();
+                  }
+                  else {
+                        fragmentManager.popBackStack();
+                  }
+            }
+
+
+
+
+                        Use this in the layout file to be the tag that is replaced
+
+            <FrameLayout
+                  xmlns:android="http://schemas.android.com/apk/res/android"
+                  android:id="@+id/fragment_container"
+                  android:layout_width="match_parent"
+                  android:layout_height="match_parent"/>
+
+
+
+
+                  If you want to use the up navigation
+
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                  switch (item.getItemId()) {
+                        case android.R.id.home: {
+                              if (fragmentManager.getBackStackEntryCount() > 0) {
+                                    fragmentManager.popBackStack();
+                              }
+                              return true;
+                        }
+                  }
+                  return super.onOptionsItemSelected(item);
+            }
+*/
+
