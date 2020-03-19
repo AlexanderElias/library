5b5cebf6dbc82c9c298afb9fce3f2adf6ba28b60
Alexander Elias <alex.steven.elias@gmail.com>
2020-03-18 18:31:40 -0700

    darw optimizations and features

--- javascript/draw.js
+++ javascript/draw.js
@@ -1,56 +1,88 @@
 
 export default class Draw {
 
-    constructor () {
+    constructor (option = {}) {
         this.x = 0;
         this.y = 0;
         this.points = [];
         this.flag = false;
-    }
-
-    setup (canvas, option) {
-        option = option || {};
-
-        if (typeof canvas === 'string' || canvas.constructor === HTMLCanvasElement) {
-            option.canvas = canvas;
-        } else {
-            option = canvas || option;
-        }
-
+        this.width = option.width || 1;
         this.fill = option.fill || null;
         this.line = option.line || 'black';
-
-        this.width = option.width || 1;
         this.quality = option.quality || 0.5;
-        this.type = option.type || 'image/webp';
-        this.canvas = typeof option.canvas === 'string' ? document.body.querySelector(option.canvas) : option.canvas;
+        this._type = option.type || 'image/webp';
+        this._up = this.tick.bind(this, this.change.bind(this, 'up'));
+        this._out = this.tick.bind(this, this.change.bind(this, 'out'));
+        this._down = this.tick.bind(this, this.change.bind(this, 'down'));
+        this._move = this.tick.bind(this, this.change.bind(this, 'move'));
+    }
 
-        if (!option.canvas) {
-            throw new Error('Signer - canvas option required');
-        }
+    type (type) {
+        if (typeof type !== 'string') throw new Error('type invalid');
+        this._type = type;
+        return this;
+    }
 
-        this.context = this.canvas.getContext('2d', {
+    canvas (canvas) {
+        canvas = typeof canvas === 'string' ? document.body.querySelector(canvas) : canvas;
+
+        if (!(canvas instanceof HTMLCanvasElement)) throw new Error('canvas invalid');
+
+        this._canvas = canvas;
+
+        this._context = this._canvas.getContext('2d', {
             desynchronized: true,
             preserveDrawingBuffer: true
         });
 
-        this.canvas.style.cursor = 'crosshair';
-        this.canvas.width = this.canvas.parentElement.clientWidth;
-        this.canvas.height = this.canvas.parentElement.clientHeight;
+        this._canvas.width = this._canvas.parentElement.clientWidth;
+        this._canvas.height = this._canvas.parentElement.clientHeight;
 
         if (this.fill) {
-            this.context.fillStyle = this.fill;
-            this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+            this._context.fillStyle = this.fill;
+            this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
         }
 
-        this.canvas.addEventListener('mouseup', this.tick.bind(this, this.change.bind(this, 'up')), false);
-        this.canvas.addEventListener('mouseout', this.tick.bind(this, this.change.bind(this, 'out')), false);
-        this.canvas.addEventListener('mousedown', this.tick.bind(this, this.change.bind(this, 'down')), false);
-        this.canvas.addEventListener('mousemove', this.tick.bind(this, this.change.bind(this, 'move')), false);
+        return this;
+    }
 
-        this.canvas.addEventListener('touchend', this.tick.bind(this, this.change.bind(this, 'up')), false);
-        this.canvas.addEventListener('touchmove', this.tick.bind(this, this.change.bind(this, 'move')), false);
-        this.canvas.addEventListener('touchstart', this.tick.bind(this, this.change.bind(this, 'down')), false);
+    unlisten () {
+        this._canvas.style.cursor = 'default';
+
+        this._canvas.removeEventListener('mouseup', this._up);
+        this._canvas.removeEventListener('mouseout', this._out);
+        this._canvas.removeEventListener('mousedown', this._down);
+        this._canvas.removeEventListener('mousemove', this._move);
+        this._canvas.removeEventListener('touchend', this._up);
+        this._canvas.removeEventListener('touchmove', this._move);
+        this._canvas.removeEventListener('touchstart', this._down);
+
+        return this;
+    }
+
+    listen () {
+        this._canvas.style.cursor = 'crosshair';
+
+        this._canvas.addEventListener('mouseup', this._up, false);
+        this._canvas.addEventListener('mouseout', this._out, false);
+        this._canvas.addEventListener('mousedown', this._down, false);
+        this._canvas.addEventListener('mousemove', this._move, false);
+        this._canvas.addEventListener('touchend', this._up, false);
+        this._canvas.addEventListener('touchmove', this._move, false);
+        this._canvas.addEventListener('touchstart', this._down, false);
+        
+        return this;
+    }
+
+    erase () {
+        this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+
+        if (this.fill) {
+            this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+            this._context.fillStyle = this.fill;
+        }
+
+        return this;
     }
 
     tick (method, events) {
@@ -81,40 +113,30 @@ export default class Draw {
         if (!this.flag) return;
         this.points.push({ x, y });
 
-        this.context.beginPath();
-        this.context.moveTo(this.points[0].x, this.points[0].y);
+        this._context.beginPath();
+        this._context.moveTo(this.points[0].x, this.points[0].y);
 
         for (var i = 1; i < this.points.length; i++) {
-            this.context.lineTo(this.points[i].x, this.points[i].y);
+            this._context.lineTo(this.points[i].x, this.points[i].y);
         }
 
-        this.context.strokeStyle = this.line;
-        this.context.shadowColor = this.line;
-        this.context.lineWidth = this.width;
-        this.context.lineCap = 'round';
-        this.context.lineJoin = 'round';
-        this.context.shadowBlur = 1;
-        this.context.stroke();
-        this.context.closePath();
+        this._context.strokeStyle = this.line;
+        this._context.shadowColor = this.line;
+        this._context.lineWidth = this.width;
+        this._context.lineCap = 'round';
+        this._context.lineJoin = 'round';
+        this._context.shadowBlur = 1;
+        this._context.stroke();
+        this._context.closePath();
     }
 
     change (type, e) {
-        const bounds = this.canvas.getBoundingClientRect();
+        const bounds = this._canvas.getBoundingClientRect();
         const tx = e.touches ? e.touches[0].clientX : e.clientX;
         const ty = e.touches ? e.touches[0].clientY : e.clientY;
         const x = tx - bounds.left;
         const y = ty - bounds.top;
         this[type](x, y);
-    }
-
-    erase () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        if (this.fill) {
-            this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-            this.context.fillStyle = this.fill;
-        }
-
     }
 
     async reader (blob, type) {
@@ -140,16 +162,16 @@ export default class Draw {
     async blob (blob) {
         if (blob) {
             const image = await this.image(blob);
-            this.context.drawImage(image, 0, 0);
+            this._context.drawImage(image, 0, 0);
         } else {
-            return new Promise(resolve => this.canvas.toBlob(resolve, this.type, this.quality));
+            return new Promise(resolve => this._canvas.toBlob(resolve, this._type, this.quality));
         }
     }
 
     async url (url) {
         if (url) {
             const image = await this.image(url);
-            this.context.drawImage(image, 0, 0);
+            this._context.drawImage(image, 0, 0);
         } else {
             const blob = await this.blob();
             return this.reader(blob, 'readAsDataURL');
@@ -175,7 +197,7 @@ export default class Draw {
     //         const index = url.indexOf('base64,')+7;
     //         url = `${url.slice(0, index)}${this.base64UrlToBase64(url.slice(index))}`;
     //         const image = await this.image(url);
-    //         this.context.drawImage(image, 0, 0);
+    //         this._context.drawImage(image, 0, 0);
     //     } else {
     //         const blob = await this.blob();
     //         const binary = await this.reader(blob, 'readAsBinaryString');
